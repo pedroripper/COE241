fd26e1bfac0303b7ef56d81ba1379324d4d3f2cc
Pedro Ripper <pedros.ripper@globo.com>
2018-09-05 14:48:33 -0300

    coinToss .dat files

---
+++ 2nd Handout/coinToss.py
@@ -0,0 +1,46 @@
+import random
+
+class coinToss(object):
+	def __init__(self,player1,player2):
+		self.player1 = player1
+		self.player2 = player2
+
+
+	def round(self):
+		events = ["heads", "tails"]
+		coinFace = random.choice(events)
+		if self.player1 == 0 or self.player2 == 0 :
+			return self.player1, self.player2
+		else:
+			if coinFace == "heads":
+				self.player1 += 100
+				self.player2 -= 100
+			else:
+				self.player2 += 100
+				self.player1 -= 100
+			return self.player1, self.player2
+		
+
+
+
+
+game = coinToss(1000,1000)
+file1 = open("player1.dat","w+")
+file1.write("#Player 1 \n")
+file1.write("#Round Money \n")
+file2 = open("player2.dat", "w+")
+file2.write("#Player 2 \n")
+file2.write("#Round Money \n")
+for i in range(0, 1001):
+	game.player1,game.player2 = game.round()
+	file1.write(str(i) + " " + str(game.player1) + "\n")
+	file2.write(str(i) + " " + str(game.player2) + "\n")
+
+
+
+	
+
+
+
+
+
